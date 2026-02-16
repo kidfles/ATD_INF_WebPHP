87362eb4222db6dfe71124d748d5bb6b3bc1d4e9
kidfles <166709765+kidfles@users.noreply.github.com>
2026-02-16 20:14:18 +0100

    bootstrap

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,3 +1,9 @@
+/**
+ * In dit bestand laden we de HTTP-bibliotheek axios, die wordt gebruikt om verzoeken
+ * naar de Laravel back-end te sturen. Axios is geconfigureerd om de CSRF-token
+ * automatisch mee te sturen als header voor beveiligde verzoeken.
+ */
+
 import axios from 'axios';
 window.axios = axios;
 
